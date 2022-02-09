52e5eb19da1a6010704398eb1522f98108170c82
Alfred <ya95123@gmail.com>
2022-02-09 16:00:35 +0800

    class component

--- 220208_2_startReact/my-app/src/App.js
+++ 220208_2_startReact/my-app/src/App.js
@@ -1,27 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react/cjs/react.production.min';
 
-function App() {
-  const state = {
-    count: 1
+// *create react app 首先出來的寫法，待確認是否有新寫法
+// function App() {
+//   const state = {
+//     count: 1
+//   }
+
+//   const addCount = () => {
+//     state.count += 1
+//     console.log(state.count)
+//   }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>{state.count}</h1>
+//         <button onClick={addCount}>Add</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// *可執行寫法 
+class App extends Component {
+  state = {
+    count: 1,
+  }
+  addCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+    console.log(this.state.count + 1)
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{state.count}</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{this.state.count}</h1>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.addCount}>Add</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
