83b95dd582cbbbd7712768ba2f596f1c17577bdf
Alfred <ya95123@gmail.com>
2022-02-09 16:00:08 +0800

    add simple render

--- 220208_2_startReact/my-app/src/index.js
+++ 220208_2_startReact/my-app/src/index.js
@@ -11,6 +11,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// *簡易寫法
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById("root")
+// )
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
