9172f6e4a4767706789887069bc4bc3f0c648bb5
Alfred <ya95123@gmail.com>
2022-02-16 17:42:25 +0800

    doc tree practice OK

--- 220216_1_docTreeWork/toggole-doc/src/App.js
+++ 220216_1_docTreeWork/toggole-doc/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import ListNode from './ListNode'
 import sportsList from './sportsList.json'
+import sportsList2 from './sportsList2.json'
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ListNode {...sportsList} />
+        {/* <ListNode {...sportsList} /> */}
+        <ListNode {...sportsList2} />
       </div>
     )
   }
--- 220216_1_docTreeWork/toggole-doc/src/ListNode.js
+++ 220216_1_docTreeWork/toggole-doc/src/ListNode.js
@@ -1,22 +1,72 @@
 import React, { Component } from 'react'
+import "./list.css"
 
 export default class ListNode extends Component {
-  render() {
-    const { name, list, sports } = this.props
+  state = {
+    open: false
+  }
 
+  toggleOpen = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+
+  render() {
+    const { name, list } = this.props
+    const { open } = this.state
+
+    // !到最底層
     if (!list) {
-      return <h2>{name}</h2>
+      return <li>{name}</li>
     }
 
     return (
       <div>
-        <h2>{name}</h2>
-        <ul>
-          {/* TODO render */}
-          {list.map(item => (
-            <li key={item.sports}>{item.sports}</li>
-          ))}
-        </ul>
+        {/* List2 */}
+        <h2
+          onClick={this.toggleOpen}
+          className={open ? "topic open" : "topic"}
+        >
+          {name}
+        </h2>
+
+        {!open ? null : (
+          <ul>
+            {
+              list.map(item => (
+                // !logic：會根據 json 同樣的名稱繼續往下挖到底
+                <ListNode {...item} key={item.name} />
+              ))
+            }
+          </ul>
+        )
+        }
+
+
+        {/* List1 */}
+        {/* <h2>{name}</h2>
+        {
+          list.map(item => (
+            <div key={item.sports} className="nameList">
+              <h3
+                onClick={this.toggleOpen}
+                className={open ? "topic open" : "topic"}
+              >
+                {item.sports}
+              </h3>
+
+              {
+                !open ? null : (
+                  <p>
+                    {item.players}
+                  </p>
+                )
+              }
+            </div>
+          ))
+        } */}
       </div>
     )
   }
