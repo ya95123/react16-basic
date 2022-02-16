483e564453f7d63d5c53c20eb103f4b2de3de76c
Alfred <ya95123@gmail.com>
2022-02-16 13:49:58 +0800

    prcatice temp

--- 220216_1_docTreeWork/toggole-doc/src/ListNode.js
+++ 220216_1_docTreeWork/toggole-doc/src/ListNode.js
@@ -13,7 +13,9 @@ export default class ListNode extends Component {
         <h2>{name}</h2>
         <ul>
           {/* TODO render */}
-
+          {list.map(item => (
+            <li key={item.sports}>{item.sports}</li>
+          ))}
         </ul>
       </div>
     )
